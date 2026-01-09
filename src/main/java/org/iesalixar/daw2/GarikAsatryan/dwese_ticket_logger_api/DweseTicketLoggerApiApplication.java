531421fb53f664e8ca0b988f561de88a564fa7d2
Garik Asatryan <asa7ur@proton.me>
2026-01-09 10:57:55 +0100

    Minor changes

--- src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_ticket_logger_api/DweseTicketLoggerApiApplication.java
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_ticket_logger_api/DweseTicketLoggerApiApplication.java
@@ -2,12 +2,16 @@ package org.iesalixar.daw2.GarikAsatryan.dwese_ticket_logger_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class DweseTicketLoggerApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DweseTicketLoggerApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DweseTicketLoggerApiApplication.class, args);
+    }
 
 }
