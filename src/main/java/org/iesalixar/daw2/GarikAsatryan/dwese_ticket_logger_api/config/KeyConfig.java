a0b3d1e85b3ba54666b9753e61fb43d74622687d
Garik Asatryan <asa7ur@proton.me>
2026-01-22 15:40:12 +0100

    Minor changes

--- src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_ticket_logger_api/config/KeyConfig.java
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_ticket_logger_api/config/KeyConfig.java
@@ -1,6 +1,6 @@
 package org.iesalixar.daw2.GarikAsatryan.dwese_ticket_logger_api.config;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
