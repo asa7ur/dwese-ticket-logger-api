c44f805ecbcc15f605f218f18239ab845e48d11b
Garik Asatryan <asa7ur@proton.me>
2026-01-22 14:58:35 +0100

    Adding Security

--- src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_ticket_logger_api/controllers/FileStorageController.java
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_ticket_logger_api/controllers/FileStorageController.java
@@ -20,7 +20,7 @@ import java.nio.file.Paths;
 
 
 @RestController
-@RequestMapping("/api/images")
+@RequestMapping("/api/v1/images")
 public class FileStorageController {
     @Value("${UPLOAD_PATH}")
     private String uploadPath;
