5075f3d705e8a002bd32df3226e5eb26e1b631c1
Garik Asatryan <asa7ur@proton.me>
2026-01-27 10:11:41 +0100

    Minor changes

--- src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_ticket_logger_api/controllers/FileStorageController.java
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_ticket_logger_api/controllers/FileStorageController.java
@@ -20,7 +20,7 @@ import java.nio.file.Paths;
 
 
 @RestController
-@RequestMapping("/api/v1/images")
+@RequestMapping("/api/images")
 public class FileStorageController {
     @Value("${UPLOAD_PATH}")
     private String uploadPath;
