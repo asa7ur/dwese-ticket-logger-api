79cb6b2e754351334a896587a646793732046169
Garik Asatryan <asa7ur@proton.me>
2026-02-09 17:15:01 +0100

    Added Websocket

---
+++ src/main/java/org/iesalixar/daw2/GarikAsatryan/dwese_ticket_logger_api/entities/Notification.java
@@ -0,0 +1,28 @@
+package org.iesalixar.daw2.GarikAsatryan.dwese_ticket_logger_api.entities;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "notifications")
+public class Notification {
+    @Id
+    private String id;
+
+    private String subject;
+
+    private String message;
+
+    private boolean read;
+
+    private Instant createdAt = Instant.now();
+}
